75186002d469fcb0c7b925a1cf08c8369ef4ae11
Ian Quinn <ianquinn22@gmail.com>
2025-12-01 14:56:57 -0400

    Starting

---
+++ src/OrdersSolution/Orders.Api/Endpoints/Orders/OrderApi.cs
@@ -0,0 +1,67 @@
+﻿using Orders.Api.Endpoints.Orders.Services;
+using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Api.Endpoints.Orders
+{
+    public static class OrderApi
+    {
+        extension (IServiceCollection services)
+        {
+            public IServiceCollection AddOrders()
+            {
+                services.AddScoped<CardProcessor>();
+                return services;
+            }
+        }
+        extension (IEndpointRouteBuilder builder)
+        {
+            public IEndpointRouteBuilder MapOrders()
+            {
+                var ordersGroup = builder.MapGroup("/orders");
+
+                ordersGroup.MapGet("/", () => "Your Orders");
+
+                ordersGroup.MapPost("/", async (ShoppingCartRequest request, CardProcessor cardProcessor, CancellationToken token) =>
+                {
+                    // That transaction list
+                    // validate the request
+
+                    // do the early bound stuff we can do, then schedule the rest for later
+                    // come back to this..
+                    //arrange shipping
+                    var shippingTask = Task.Delay(1000); // come back to this list when this is done
+                    // charge card
+
+                    //async tasks are not "fire and forget" - they are "fire and hope nothing bad happens"
+                    var cardTask = cardProcessor.ProcessCardAsync(request.CustomerName, token);
+                    // etc.
+                    // save it...
+                    // then
+                    var order = new Order
+                    {
+                        Id = Guid.NewGuid(),
+                        Total = request.Amount * 1.13M
+                    };
+                    return TypedResults.Ok(order); // the caller only gets this.
+                });
+
+                return builder;
+            }
+        }
+    }
+
+    public record ShoppingCartRequest
+    {
+        []
+        public decimal Amount { get; set; }
+        
+        [Required, MinLength(3), MaxLength(100)]
+        public string CustomerName { get; set; } = string.Empty;
+    }
+
+    public record Order
+    {
+        public Guid Id { get; set; }
+        public decimal  Total { get; set; }
+    }
+}
---
+++ src/OrdersSolution/Orders.Api/Endpoints/Orders/Services/CardProcessor.cs
@@ -0,0 +1,13 @@
+﻿namespace Orders.Api.Endpoints.Orders.Services
+{
+    public class CardProcessor
+    {
+
+        public async Task<string> ProcessCardAsync(string customerName, CancellationToken token)
+        {
+            // write code to do this later
+            await Task.Delay(1000);
+            return "33333999"; // confirmation number
+        }
+    }
+}
